798675beb65218ae74cf95185143a1d3cb0bd56d
WiseDemon <zloradny.demon@gmail.com>
2021-03-03 23:49:27 +0300

    Update __init__.py
    Forgot to fix some links.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ['storage', 'redis_command_parser', 'server_protocol',
            'client_protocol', 'redis_protocol', 'redis_protocol_error', 'redis_data_parser',
-           'redis_data_parser.py', 'redis_protocol.py', 'redis_encoder']
+           'redis_data_parser', 'redis_protocol', 'redis_encoder','redis_pattern_matching']
